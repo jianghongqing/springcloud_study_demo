3ba36fee9019e9611b8e4eaf73b37e0b5866edff
wangsong <vmaps@qq.com>
2018-06-13 11:30:16 +0800

    集成服务监控

---
+++ springcloud-app-zipkin/src/main/java/com/wangsong/ZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.wangsong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+}
