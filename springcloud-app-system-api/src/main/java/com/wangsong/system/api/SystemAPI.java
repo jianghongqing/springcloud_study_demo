4a086de3a2bc3e2421301c729c83f9cd1b42bf35
wangsong <vmaps@qq.com>
2018-10-24 19:25:02 +0800

    swagger lombok 优化结构

--- springcloud-app-system-api/src/main/java/com/wangsong/system/api/SystemAPI.java
+++ springcloud-app-system-api/src/main/java/com/wangsong/system/api/SystemAPI.java
@@ -1,23 +1,22 @@
 package com.wangsong.system.api;
 
-import java.util.List;
-
 import com.wangsong.common.model.Result;
+import com.wangsong.system.model.Resources;
+import com.wangsong.system.model.User;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.wangsong.system.model.Resources;
-import com.wangsong.system.model.User;
+import java.util.List;
 
 @FeignClient("springcloud-app-system")
 public interface SystemAPI {
 
 
     @RequestMapping(value = "/springcloud-app-system/api/getUser", method = RequestMethod.POST)
-    Result<User> getUser(@RequestBody  User u);
+    Result<User> getUser(@RequestBody User u);
 
     @RequestMapping(value = "/springcloud-app-system/api/getResources", method = RequestMethod.POST)
-    Result<List<Resources>> getResources(@RequestBody  Resources r);
+    Result<List<Resources>> getResources(@RequestBody Resources r);
 }
--- springcloud-app-system/src/main/java/com/wangsong/system/api.impl/SystemAPIImpl.java
+++ springcloud-app-system/src/main/java/com/wangsong/system/api.impl/SystemAPIImpl.java
@@ -11,7 +11,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -20,27 +23,27 @@ import java.util.List;
 @RequestMapping("/api")
 @Api(value = "SystemAPI")
 public class SystemAPIImpl extends BaseController {
-	
-		
-	@Autowired
-	private UserService userService;
 
-	@Autowired
-	private ResourcesService resourcesService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ResourcesService resourcesService;
 
     @ApiOperation(value = "获取用户", httpMethod = "POST")
     @RequestMapping(value = "/getUser", method = RequestMethod.POST)
     @ResponseBody
-    public Result<User> getUser(@ModelAttribute @RequestBody  User u) {
-    	return new Result(CodeEnum.SUCCESS.getCode(), userService.findTByT(u));
+    public Result<User> getUser(@RequestBody User u) {
+        return new Result(CodeEnum.SUCCESS.getCode(), userService.findTByT(u));
     }
 
     @ApiOperation(value = "获取权限", httpMethod = "POST")
     @RequestMapping(value = "/getResources", method = RequestMethod.POST)
     @ResponseBody
-    public Result<List<Resources>> getResources(@ModelAttribute @RequestBody  Resources r) {
-    	return new Result(CodeEnum.SUCCESS.getCode(), resourcesService.findTByT(r));
+    public Result<List<Resources>> getResources(@RequestBody Resources r) {
+        return new Result(CodeEnum.SUCCESS.getCode(), resourcesService.findTByT(r));
     }
 
-    
+
 }
